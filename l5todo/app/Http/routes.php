d7143c7a5906fe6cee56b10a5ac54de0946998fb
Harrison Lin <harrisonllin@gmail.com>
2015-07-13 01:11:46 -0700

    Added models and controllers to project

--- l5todo/app/Http/routes.php
+++ l5todo/app/Http/routes.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('projects', 'ProjectsController');
+Route::resource('projects.tasks', 'TasksController');
+
+Route::bind('tasks', function($value, $route) {
+	return App\Task::whereSlug($value)->first();
+});
+Route::bind('projects', function($value, $route) {
+	return App\Project::whereSlug($value)->first();
+});

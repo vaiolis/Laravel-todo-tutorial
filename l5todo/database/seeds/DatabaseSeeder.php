57d0034ff67317f1d1415dc833de7cd050209560
Harrison Lin <harrisonllin@gmail.com>
2015-07-13 00:59:03 -0700

    Created database migrations and seeds
    Updated gitignore, created database migrations, set up database, then
    seeded database tables.

--- l5todo/database/seeds/DatabaseSeeder.php
+++ l5todo/database/seeds/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+	// $this->call(UserTableSeeder::class);
+	$this->call('ProjectsTableSeeder');
+	$this->call('TasksTableSeeder');
 
         Model::reguard();
     }
